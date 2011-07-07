11a7caef802777c420ec0ed3f61662a2592b406d
Pierre <pierre.basile@gmail.com>
2011-07-07 23:11:03 +0200

    update form with js

--- db/20110625111422_add_discount_to_price.rb
+++
@@ -1,9 +0,0 @@
-class AddDiscountToPrice < ActiveRecord::Migration
-  def self.up
-    add_column :specials, :discount, :integer
-  end
-
-  def self.down
-    remove_column :specials, :discount, :integer
-  end
-end
